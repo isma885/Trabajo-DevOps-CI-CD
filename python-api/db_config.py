d9e8bb5b4200234723fdc522b67be8d4951e14fa
Agostina Avalle <avalleag@gmail.com>
2024-11-09 16:16:04 -0300

    saco secret keyword de db_config.py

--- python-api/db_config.py
+++ python-api/db_config.py
@@ -2,14 +2,6 @@ from app import app
 from flaskext.mysql import MySQL
 import os
 mysql = MySQL()
- 
- 
-# MySQL configurations
-# app.config['MYSQL_DATABASE_USER'] = 'testuser'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'testuserpass'
-# app.config['MYSQL_DATABASE_DB'] = 'test'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# app.config['MYSQL_DATABASE_PORT'] = 3306
 
 app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_DATABASE_USER')
 app.config['MYSQL_DATABASE_PASSWORD'] =  os.environ.get('MYSQL_DATABASE_PASSWORD')
@@ -18,5 +10,3 @@ app.config['MYSQL_DATABASE_HOST'] =  os.environ.get('MYSQL_DATABASE_HOST')  or '
 app.config['MYSQL_DATABASE_PORT'] =  int(os.environ.get('MYSQL_DATABASE_PORT')) or 3306
 print(app.config)
 mysql.init_app(app)
-# conn = mysql.connect()
-# conn.close()

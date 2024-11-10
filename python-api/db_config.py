109e2bbcae476831d18b31511972677d6cca6a8c
Ismael Carrizo <89312@sistemas.frc.utn.edu.ar>
2024-11-10 00:35:43 -0300

    prueba 7

--- python-api/db_config.py
+++ python-api/db_config.py
@@ -4,9 +4,14 @@ import os
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_DATABASE_USER')
-app.config['MYSQL_DATABASE_PASSWORD'] =  os.environ.get('MYSQL_DATABASE_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] =  os.environ.get('MYSQL_DATABASE_DB')
-app.config['MYSQL_DATABASE_HOST'] =  os.environ.get('MYSQL_DATABASE_HOST')  or 'localhost'
-app.config['MYSQL_DATABASE_PORT'] =  int(os.environ.get('MYSQL_DATABASE_PORT')) or 3306
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_DATABASE_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DATABASE_DB')
+mysql_host = os.environ.get('MYSQL_DATABASE_HOST')
+app.config['MYSQL_DATABASE_HOST'] = mysql_host or 'localhost'
+
+
+mysql_port = os.environ.get('MYSQL_DATABASE_PORT')
+app.config['MYSQL_DATABASE_PORT'] = int(mysql_port) if mysql_port else 3306
+
 print(app.config)
 mysql.init_app(app)

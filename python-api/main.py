6b22261acc8c5d4ec8abee75e0d0c2d580e9db18
Ismael Carrizo <89312@sistemas.frc.utn.edu.ar>
2024-11-21 01:22:41 -0300

    prueba 22

--- python-api/main.py
+++ python-api/main.py
@@ -140,20 +140,8 @@ def not_found(error=None):
 
 @app.route('/health', methods=['GET'])
 def health():
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("SELECT 1")
-        cursor.fetchone()
-        conn.close()
-
-        return jsonify({"status": "healthy"}), 200
-
-    except Exception as e:
-        print(f"Error: {e}")
-        return jsonify({"status": "unhealthy", "message": str(e)}), 500
-
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == "__main__":
     # app.run()
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.11')
